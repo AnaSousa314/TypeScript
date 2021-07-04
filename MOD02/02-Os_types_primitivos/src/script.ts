c24107c724708d88e22b50101a6dfa6ed061178a
Ana.Sousa <anasousa314@outlook.com>
2021-07-04 17:40:41 -0300

    MOD02 aula 02 sobre tipos primitivos no typescript, como tipar as variaveis no typescript

---
+++ MOD02/02-Os_types_primitivos/src/script.ts
@@ -0,0 +1,10 @@
+let nome: string = "Ana";//se tentar colocar um valor de tipo diferente de string, dara erro
+
+let idade = 25;//a idade esta sendo colocada no proprio codigo, logo não se faz necessário especificar a tipagem.
+
+/* Coloque tipagem somente no que for realmente necessario; coloque as tipagens em letras minúsculas */
+
+let programador: boolean = true;
+
+
+
