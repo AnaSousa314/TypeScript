da800c50bc163fa6f76e0559f2c408c65a83bc05
Ana.Sousa <anasousa314@outlook.com>
2021-07-09 14:26:17 -0300

    MOD03 aula 04 sobre para que serve o target do tsconfig e como usa-lo

---
+++ MOD03/04-Entendendo_o_Target/src/script.js
@@ -0,0 +1,4 @@
+"use strict";
+/*
+ O target é onde colocamos a versão do JS que queremos compilar. Por padrão ele deixa no es5, mas ele também converte para ES2015 até ES2021, também ES3, ES6 e ESNext.
+*/ 
---
+++ MOD03/04-Entendendo_o_Target/src/script.ts
@@ -0,0 +1,4 @@
+/*
+No tsconfig
+ O target é onde colocamos a versão do JS que queremos compilar/converter. Por padrão ele deixa no es5, mas ele também converte para ES2015 até ES2021, também ES3, ES6 e ESNext.
+*/
